52c1a80ee38ab95266181ebccf2f08365bdb4c37
bchadwic <bchadwick@mail.greenriver.edu>
2021-06-21 22:15:06 -0700

    Initial commit

---
+++ word-bubble/src/main/java/com/wordbubble/wordbubble/WordBubbleApplication.java
@@ -0,0 +1,13 @@
+package com.wordbubble.wordbubble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordBubbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordBubbleApplication.class, args);
+	}
+
+}
---
+++ word-bubble/src/test/java/com/wordbubble/wordbubble/WordBubbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordbubble.wordbubble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordBubbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
